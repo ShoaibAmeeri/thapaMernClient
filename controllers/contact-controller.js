3dca942064c37ddc65f13438ef5f08b538d7dab3
unknown <shoaibameeri56@gmail.com>
2024-02-28 12:34:29 +0500

    contact form solved

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -6,10 +6,11 @@ const Contact = require("../models/contact-model")
 const contactForm = async (req, res) => {
     try {
        const response = req.body
+       console.log(response)
        await Contact.create(response)
-       return res.status(200).json({message : "message send successfully"})
+       return res.status(200).json({message : "message send successfully" , response})
     } catch (error) {
-        res.status(500).json({err : error.message})
+        res.status(500).json({err : error.message , msg: "message not delivered"})
     }
 
 }
